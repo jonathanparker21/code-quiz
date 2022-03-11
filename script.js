ad447dc7048491e50db8364b9ac6ad01105c769b
Jonathan Parker <jprkr21@gmail.com>
2022-03-10 21:16:23 -0600

    Pseudocode for JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+write array of questions
+hide end page & high scores
+add event listener to start the game
+write function to start the quiz
+write function to show the questions
+write function to check the answers
+display the score
+stop the quiz
+save user initials
+*/
